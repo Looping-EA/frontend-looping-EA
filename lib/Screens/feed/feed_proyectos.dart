00e9296f5166aae67e40267dddc5280f32a3474b
MARC XAPELLI JAIME <marc.xapelli@estudiantat.upc.edu>
2021-05-07 13:56:24 +0200

    Update feed_proyectos.dart

--- lib/Screens/feed/feed_proyectos.dart
+++ lib/Screens/feed/feed_proyectos.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../Models/project.dart';
+import '../../styles.dart';
 
 void main() => runApp(FeedProyectos());
 
@@ -22,7 +23,7 @@ class _FeedProyectosState extends State<FeedProyectos> {
       title: 'Feed Proyectos',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Proyectos'),
+          title: Text('Proyectos' , style: Styles.subtitle),
         ),
         body: ListView.builder(
             itemCount: projects.length,
@@ -30,10 +31,18 @@ class _FeedProyectosState extends State<FeedProyectos> {
               return ListTile(
                   title: Text(projects[index].name),
                   subtitle: Text(
-                    projects[index].owners.toString(),
-                  ));
+                    projects[index].owners.toString()),
+                  onTap: () {
+        //_navigationToProjectDetail(context, project);
+        },
+                  );
             }),
       ),
+  // Al clicar en un proyecto entrar en el, como no hay pagina de proyecto todavia no esta operativa
+  //void _navigationToProjectDetail(BuildContext context, Project project) {
+  //Navigator.push(context,
+  //MaterialPageRoute(builder: (context) => ProjectDetail(project)));
+  //}
     );
   }
 }
