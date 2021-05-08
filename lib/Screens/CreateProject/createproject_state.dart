c353a1e440702f0a9cc031a753ccea6bb12d7b11
MARC XAPELLI JAIME <marc.xapelli@estudiantat.upc.edu>
2021-05-08 18:47:49 +0200

    Create Project

---
+++ lib/Screens/CreateProject/createproject_state.dart
@@ -0,0 +1,4 @@
+import 'package:frontend_looping_ea/Screens/CreateProject/createproject_screen.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class CreateProjectState extends State<CreateProjectScreen> {}
