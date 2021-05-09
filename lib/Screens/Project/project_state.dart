f1ed4eaa4b97d15381f7362495869be5cb60eecc
MARC XAPELLI JAIME <marc.xapelli@estudiantat.upc.edu>
2021-05-09 03:02:52 +0200

    Update project_state.dart

--- lib/Screens/Project/project_state.dart
+++ lib/Screens/Project/project_state.dart
@@ -11,6 +11,60 @@ class ProjectState extends State<ProjectScreen> {
   ProjectState(this.project);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Center(
+            child: Container(
+                width: width,
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '${project.name}',
+                            style: Styles.title,
+                          ),
+                          SizedBox(),
+                          SizedBox(
+                              width: width * 0.25,
+                              height: height * 0.1,
+                              child: ElevatedButton(
+                                  onPressed: _onPressButton,
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Styles.colorBackground,
+                                  ),
+                                  child: Text(
+                                    'Apply',
+                                    style: Styles.button_big,
+                                  )))
+                        ]),
+                    SizedBox(height: height * 0.1),
+                    Text(
+                      '${project.description}',
+                      style: Styles.subtitle,
+                    ),
+                    SizedBox(height: height * 0.1),
+                    Text(
+                      ownersNameStringBuilder(project),
+                      style: Styles.subtitle,
+                    ),
+                  ],
+                ))));
   }
+
+  String ownersNameStringBuilder(Project x) {
+    //Builds a string with owners' names
+    String names = x.owners[0].uname;
+    if (x.owners.length > 1) {
+      for (int i = 0; i < x.owners.length; i++) {
+        names = names + ", " + x.owners[i].uname;
+      }
+    }
+    return names;
+  }
+
+  void _onPressButton() {}
 }
