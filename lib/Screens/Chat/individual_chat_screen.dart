2fd8e3029689546b608fbe5d7d06ea8bda3261b8
MARC XAPELLI JAIME <marc.xapelli@estudiantat.upc.edu>
2021-06-12 13:59:34 +0200

    Input Text updated

--- lib/Screens/Chat/individual_chat_screen.dart
+++ lib/Screens/Chat/individual_chat_screen.dart
@@ -11,7 +11,9 @@ class IndividualChat extends StatefulWidget {
 class _IndividualChatState extends State<IndividualChat> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.blueGrey.shade50,
       appBar: AppBar(
           leadingWidth: 70,
           leading: InkWell(
@@ -29,6 +31,44 @@ class _IndividualChatState extends State<IndividualChat> {
           title: Column(
             children: [Text(widget.chat.name)],
           )),
+      body: Container(
+          height: size.height,
+          width: size.width,
+          child: Stack(children: [
+            ListView(),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.fromLTRB(
+                            size.width * 0.05, 0, size.width * 0.05, 0),
+                        width: size.width * 0.75,
+                        child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.circular(size.width * 0.05)),
+                            child: TextFormField(
+                              textAlignVertical: TextAlignVertical.center,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 3,
+                              minLines: 1,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Type a message",
+                                  contentPadding:
+                                      EdgeInsets.all(size.width * 0.01)),
+                            ))),
+                    Container(
+                        height: size.height * 0.05,
+                        width: size.width * 0.1,
+                        child: ElevatedButton(
+                            onPressed: onPressed, child: Text("Send"))),
+                  ],
+                ))
+          ])),
     );
   }
+
+  void onPressed() {}
 }
