e59969d33f5c769b685a3b7d619643de2e437fcf
MARC XAPELLI JAIME <marc.xapelli@estudiantat.upc.edu>
2021-06-12 15:58:12 +0200

    user screen updated

---
+++ lib/Screens/Chat/user_card.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_looping_ea/Models/user.dart';
+
+import '../../styles.dart';
+
+class UserCard extends StatelessWidget {
+  const UserCard({Key? key, required this.contact}) : super(key: key);
+  final User contact;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {},
+        child: ListTile(
+            leading: CircleAvatar(radius: 20),
+            title: Text(contact.uname, style: Styles.projectText),
+            subtitle: Text(contact.fullname, style: TextStyle(fontSize: 13))));
+  }
+}
--- lib/Screens/Chat/users_screen.dart
+++ lib/Screens/Chat/users_screen.dart
@@ -1,3 +1,6 @@
+import 'package:frontend_looping_ea/Models/chat.dart';
+import 'package:frontend_looping_ea/Models/user.dart';
+import 'package:frontend_looping_ea/Screens/Chat/user_card.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 import 'package:flutter/material.dart';
@@ -12,34 +15,45 @@ class UsersScreen extends StatelessWidget {
   get data => null;
   @override
   Widget build(BuildContext context) {
+    List<User> contacts = [
+      User("Xape", "xape23", "Marc Xapelli", "marcxapelli@hotmail.com",
+          "45GHF%678IK"),
+      User("Luis", "luis54", "Luis Domingo", "luis.domingo@gmail.com",
+          "61HJ9LM45"),
+      User("MaFe", "mafe45", "Marc Ferre", "marcferre@upc.edu", "IYT42D678G")
+    ];
     return Scaffold(
-        body: (Container(
-            height: 200,
-            width: 200,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                      onPressed: _onPressButton,
-                      style: ElevatedButton.styleFrom(),
-                      child: Text(
-                        'Get Socket',
-                      )),
-                  SizedBox(height: 50),
-                  ElevatedButton(
-                      onPressed: _sendMessage,
-                      style: ElevatedButton.styleFrom(),
-                      child: Text(
-                        'Send Message',
-                      )),
-                ]))));
+      body:
+          //Column(children: [
+          ListView.builder(
+        itemBuilder: (context, index) => UserCard(contact: contacts[index]),
+        itemCount: contacts.length,
+      ),
+      //Container(
+      //child: Column(
+      //mainAxisAlignment: MainAxisAlignment.center,
+      //children: <Widget>[
+      //ElevatedButton(
+      // onPressed: _onPressButton,
+      //style: ElevatedButton.styleFrom(),
+      //child: Text(
+      // 'Get Socket',
+      //)),
+      //SizedBox(height: 50),
+      //ElevatedButton(
+      // onPressed: _sendMessage,
+      // style: ElevatedButton.styleFrom(),
+      // child: Text(
+      //   'Send Message',
+      // )),]))])
+    );
   }
 
   void _onPressButton() async {
     socket.connect();
-    socket.on("connect", (_) => {print("You are connected")});
+    socket.on("connect", (_) => {print("You are connected.")});
     socket.on(
-        'disconnect', (_) => print('You have disconnected from the server'));
+        "disconnect", (_) => print('You have disconnected from the server.'));
     socket.on("new message", (data) => _addChatMessage(data));
     // Dart client
   }
