f4b4bde738b1cee7ca3e54d15417432914f0a153
MARC XAPELLI JAIME <marc.xapelli@estudiantat.upc.edu>
2021-06-20 00:05:24 +0200

    Update chat.dart

--- lib/Models/chat.dart
+++ lib/Models/chat.dart
@@ -3,22 +3,33 @@ import '../Models/message.dart';
 
 class Chat {
   String name;
-  String description;
-  List<User> members = [];
-  List<Message> messages = [];
+  //String icon;
+  bool isGroup;
+  String time;
+  String currentMessage;
+  int id;
+  //List<User> members = [];
+  //List<Message> messages = [];
 
-  Chat(this.name, this.description, this.members, this.messages);
+  Chat(
+    this.name,
+    //this.icon,
+    this.isGroup,
+    this.time,
+    this.currentMessage,
+    this.id,
+  );
 
   factory Chat.fromJson(dynamic json) {
-    var memberObjsJson = json['members'] as List;
-    List<User> _members =
-        memberObjsJson.map((memberJson) => User.fromJson(memberJson)).toList();
-
-    var messagesObjsJson = json['messages'] as List;
-    List<Message> _messages = messagesObjsJson
-        .map((messageJson) => Message.fromJson(messageJson))
-        .toList();
-    return Chat(json['name'] as String, json['description'] as String, _members,
-        _messages);
+    //var memberObjsJson = json['members'] as List;
+    //List<User> _members =
+    //memberObjsJson.map((memberJson) => User.fromJson(memberJson)).toList();
+    return Chat(
+      json['name'] as String,
+      json['isGroup'] as bool,
+      json['time'] as String,
+      json['currentMessage'] as String,
+      json['id'] as int,
+    );
   }
 }
